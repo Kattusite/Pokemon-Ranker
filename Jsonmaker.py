9d674538f1184ebdd741c27e3a9c092fd81ab860
Justin <justinkevinwoods@gmail.com>
2021-09-29 00:00:20 -0400

    Optimized getname function and added skip
    Deleted erroneous getnametr function and changed getname function to search for b tags instead
    
    certain table rows on the national pokedex table are empty, so i added a try block to skip it

--- Jsonmaker.py
+++ Jsonmaker.py
@@ -8,7 +8,8 @@ import timeit
 index = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
 website = requests.get(index)
 table = BeautifulSoup(website.content,"html.parser")
-indexposition = 2
+indexposition = 123
+#for number in range (1,1000):
 #retrieves the link to the pokemon's individual page from the table
 row = table.find_all('tr')[indexposition]                                                                                                              
 rawlink = row.find_all('a')[0]
@@ -17,20 +18,10 @@ link = "https://bulbapedia.bulbagarden.net/" + str(rawlink.get('href'))
 pokemon = requests.get(link)
 attributes = BeautifulSoup(pokemon.content,"html.parser")
 
-#gets the pokemon name if the article uses tr formatting
-def getnametr():
-    nameraw = attributes.find_all('tr')[9]
-    namezoom = nameraw.find_all('tr')[0]
-    namezoom1 = namezoom.find_all ('a')[0]
-    name = namezoom1.get('title')
-    return name
-
 #gets the pokemon name if the article uses td formatting
-def getnametd():
-    nameraw = attributes.find_all('tr')[9]
-    namezoom = nameraw.find_all('td')[0]
-    namezoom1 = namezoom.find_all ('a')[0]
-    name = namezoom1.get('title')
+def getname():
+    nameraw = attributes.find_all('b')[1]
+    name = nameraw
     return name
 
 #gets the pokemon's types
@@ -47,8 +38,9 @@ def getgeneration():
     generation = generationzoom.get('title')
     return generation
 
-def getname():
+#skips the table row if there are no pokemon
+def tryname():
     try:
-        return getnametr()
+        return getname()
     except:
-        return getnametd()
+        pass
