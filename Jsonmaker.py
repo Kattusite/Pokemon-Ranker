e4f6bf6f55a157478a730c848f279bc7403bdace
Justin <justinkevinwoods@gmail.com>
2021-10-25 22:26:08 -0400

    Added getheight & getweight, cleaned up comments and streamlined looping
    I put the entire program in a pointless loop. I needed to do that because whenever I need to loop through all the pokemon on the website I needed to manually add the for loop line, indent everything, and then un-indent everything when I finished. To save time I'll just leave everything in a 1 iteration for loop while working on the code.

--- Jsonmaker.py
+++ Jsonmaker.py
@@ -8,60 +8,83 @@ import timeit
 index = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
 website = requests.get(index)
 table = BeautifulSoup(website.content,"html.parser")
-indexposition = 552
-#retrieves the link to the pokemon's individual page from the table
-row = table.find_all('tr')[indexposition]                                                                                                              
-rawlink = row.find_all('a')[0]
-link = "https://bulbapedia.bulbagarden.net/" + str(rawlink.get('href')) 
-#the values containing the pokemon's page  
-pokemon = requests.get(link)
-attributes = BeautifulSoup(pokemon.content,"html.parser")
+indexposition = 6
+# I need to loop through many pokemon to test them. I made the program a 1 round loop in order to avoid indenting everything whenever I need to put the code in a loop.
+for item in range (1,2):
+    #retrieves the link to the pokemon's individual page from the table
+    row = table.find_all('tr')[indexposition]                                                                                                              
+    rawlink = row.find_all('a')[0]
+    link = "https://bulbapedia.bulbagarden.net/" + str(rawlink.get('href')) 
+    #the values containing the pokemon's page  
+    pokemon = requests.get(link)
+    attributes = BeautifulSoup(pokemon.content,"html.parser")
 
-#gets the pokemon name if the article uses td formatting
-def getname():
-    nameraw = attributes.find_all('b')[1]
-    name = nameraw.get_text()
-    return name
+    def getname():
+        nameraw = attributes.find_all('b')[1]
+        name = nameraw.get_text()
+        return name
 
-#gets the pokemon's primary type
-def gettype1():
-    typeraw = attributes.find_all('b')[4]
-    type = typeraw.get_text()
-    return type
+    def gettype1():
+        typeraw = attributes.find_all('b')[4]
+        type = typeraw.get_text()
+        return type
 
-#gets the pokemon's secondary type
+    def gettype2():
+        typeraw = attributes.find_all('b')[5]
+        type = typeraw.get_text()
+        return type
 
-def gettype2():
-    typeraw = attributes.find_all('b')[5]
-    type = typeraw.get_text()
-    return type
+    def getcategory():
+        categoryraw = attributes.find_all('span')[5]
+        category = categoryraw.get_text()
+        return category
+
+    def getheight():
+        heightsearch = attributes.find("span", string="Height")
+        parent1 = heightsearch.parent
+        parent2 = parent1.parent
+        parent3 = parent2.parent
+        heightraw = parent3.findAll("td")[0]
+        height = heightraw.get_text()
+        return (height)
+
+    def getweight():
+        weightsearch = attributes.find("span", string="Weight")
+        parent1 = weightsearch.parent
+        parent2 = parent1.parent
+        parent3 = parent2.parent
+        weightraw = parent3.findAll("td")[0]
+        weight = weightraw.get_text()
+        return (weight)
+
+    def getgeneration():
+        generationraw = attributes.find_all('p')[1]
+        generationzoom = generationraw.find_all('a')[3]
+        generation = generationzoom.get('title')
+        return generation
+
+    #skips the table row if there are no pokemon
+    def tryname():
+        try:
+            return getname()
+        except:
+            pass
+
+    #easy way for me to parse through all the different tags of a page, for debugging purposes only
+    def attributesearch(targettag):
+        for item in range (1,100):
+            generation = attributes.find_all(targettag)[item]
+            print ('----------------------------')
+            print (item)
+            print (generation)
+
+    def getall():
+        print (getname())
+        print (gettype1()), print (gettype2())
+        print (getcategory())
+        print (getheight())
+        print (getweight())
+
+    
 
 
-def getcategory():
-    categoryraw = attributes.find_all('span')[5]
-    category = categoryraw.get_text()
-    return category
-
-#gets the pokemon's generation
-def getgeneration():
-    generation = attributes.find_all('p')[1]
-    generationzoom = generation.find_all('a')[3]
-    generation = generationzoom.get('title')
-    return generation
-
-#skips the table row if there are no pokemon
-def tryname():
-    try:
-        return getname()
-    except:
-        pass
-
-#easy way for me to parse through all the different tags of a page, for debugging purposes only
-def attributesearch(targettag):
-    for item in range (1,100):
-        generation = attributes.find_all(targettag)[item]
-        print ('----------------------------')
-        print (item)
-        print (generation)
-
-attributesearch("span")
